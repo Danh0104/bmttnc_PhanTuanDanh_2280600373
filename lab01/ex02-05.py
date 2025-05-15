8e045bdefcde06922667e23b6083682c249bc7ce
Danh0104 <phantuadanh1009@gmail.com>
2025-05-15 10:44:27 +0700

    lab01

---
+++ lab01/ex02-05.py
@@ -0,0 +1,6 @@
+so_gio_lam = float(input("Nhập số giờ làm mỗi tuần:"))
+luong_gio = float(input("Nhập thù lao trên mỗi giờ làm tiêu chuẩn: "))
+gio_tieu_chuan = 44 
+gio_vuot_chuan = max(0, so_gio_lam - gio_tieu_chuan)
+thuc_linh = gio_tieu_chuan * luong_gio + gio_vuot_chuan*luong_gio*1.5
+print(f"Số tiền thực lĩnh của nhân viên😊:{thuc_linh}")
